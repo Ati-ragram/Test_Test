27c575fc6b4251821878cf69d07b0e8306bc5fce
MargoPugach <marharyta.puhach@gmail.com>
2024-02-26 17:39:21 -0800

    for conflict

--- package/Main.java
+++ package/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first commit");
+        System.out.println("My first commit");
+        System.out.println("My firstrh commit");
+        System.out.println("My first4r commit");
+        System.out.println("My fr5r5irst commit");
+        System.out.println("My fe4irst commit");
+
+
+        System.out.println("My firs4etft commit");System.out.println("My firsterth commit");
+
+
     }
 }
